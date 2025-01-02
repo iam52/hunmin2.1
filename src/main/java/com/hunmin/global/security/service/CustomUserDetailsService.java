5a15a90fa314d6801a4a7e18201a5ab2da945408
iam52 <tupac52@gmail.com>
2025-01-02 20:44:47 +0900

    [mod] MemberController Token Reissue 수정

---
+++ src/main/java/com/hunmin/global/security/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.hunmin.global.security.service;
+
+import com.hunmin.domain.member.entity.Member;
+import com.hunmin.domain.member.repository.MemberRepository;
+import com.hunmin.global.security.entity.CustomUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    public CustomUserDetailsService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member memberData = memberRepository.findByEmail(email).get();
+
+        if (memberData != null) {
+            return new CustomUserDetails(memberData);
+        }
+        throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + email);
+    }
+}
